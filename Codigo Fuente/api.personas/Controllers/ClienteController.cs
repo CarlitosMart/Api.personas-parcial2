b447ec68c3f25bd0b8efaed2bad112c03933aba5
Carlos Martínez <carlosmar.py@gmail.com>
2024-05-27 20:56:46 -0400

    Codigo fuente

---
+++ Codigo Fuente/api.personas/Controllers/ClienteController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Data;
+using Services.Logica;
+
+namespace api.Clientes.Controllers
+{
+    //[ApiController]
+    //[Route("/Api/v1/")]
+    public class ClienteController : Controller
+    {
+        private ClienteService ClienteService;
+        public ClienteController(IConfiguration configuracion)
+        {
+            ClienteService = new ClienteService(configuracion.GetConnectionString("postgres"));
+        }
+        
+        [HttpGet("ListC")]
+        public async Task<ActionResult<List<ClienteModel>>> List()
+        {
+            var a=ClienteService.list();
+            return ClienteService.list();
+        }
+
+
+        // GET: ClienteController/Create
+        [HttpPost("AddC")]
+        public ActionResult Add(Repository.Data.ClienteModel Cliente)
+        {
+            ClienteService.add(Cliente);
+            return Ok(new { message = $"El Cliente con nombre {Cliente.Nombre} fue insertado correctamente!!!"});
+        }
+        [HttpPost("EditC")]
+        public ActionResult Edit(Repository.Data.ClienteModel Cliente)
+        {
+            ClienteService.edit(Cliente);
+            return Ok(new { message = $"El Cliente con nombre {Cliente.Nombre} fue actualizado correctamente!!!" });
+        }
+
+        [HttpPost("deleteC")]
+        public ActionResult delete(int id)
+        {
+            ClienteService.delete(id);
+            return Ok(new { message = $"El Cliente fue eliminado correctamente!!!" });
+        }
+
+
+        // POST: ClienteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Codigo Fuente/api.personas/Controllers/FacturaController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Data;
+using Services.Logica;
+
+namespace api.Facturas.Controllers
+{
+    //[ApiController]
+    //[Route("/Api/v1/")]
+    public class FacturaController : Controller
+    {
+        private FacturaService FacturaService;
+        public FacturaController(IConfiguration configuracion)
+        {
+            FacturaService = new FacturaService(configuracion.GetConnectionString("postgres"));
+        }
+
+        [HttpGet("ListF")]
+        public async Task<ActionResult<List<FacturaModel>>> List()
+        {
+            var a = FacturaService.list();
+            return a;
+        }
+        // GET: FacturaController/Create
+        [HttpPost("AddF")]
+        public ActionResult Add(Repository.Data.FacturaModel Factura)
+        {
+            FacturaService.add(Factura);
+            return Ok(new { message = $"La Factura con nombre {Factura.Nro_factura} fue insertado correctamente!!!"});
+        }
+        [HttpPost("EditF")]
+        public ActionResult Edit(Repository.Data.FacturaModel Factura)
+        {
+            FacturaService.edit(Factura);
+            return Ok(new { message = $"La Factura con nombre {Factura.Nro_factura} fue actualizado correctamente!!!" });
+        }
+
+        [HttpPost("deleteF")]
+        public ActionResult delete(int id)
+        {
+            FacturaService.delete(id);
+            return Ok(new { message = $"La Factura fue eliminada correctamente!!!" });
+        }
+
+
+        // POST: FacturaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FacturaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FacturaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FacturaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FacturaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
